840ebb43d799160329a4a5b2607599f31ff3006b
dbc2201 <divyanshb22@gmail.com>
2022-07-10 23:37:26 +0530

    complete the problem

---
+++ src/main/java/io/github/dbc/TowerOfHanoiSolver.java
@@ -0,0 +1,15 @@
+package io.github.dbc;
+
+public class TowerOfHanoiSolver {
+    /**
+     * This method prints the solution of the Tower of Hanoi problem.
+     *
+     * @param numberOfDisks the number of disks to be used in the game.
+     * @param fromTower     the tower to move disks from.
+     * @param toTower       the tower to move disks to.
+     * @param auxTower      the tower to move disks temporarily to.
+     */
+    public void solveTowerOfHanoi(int numberOfDisks, char fromTower, char toTower, char auxTower) {
+
+    }
+}
